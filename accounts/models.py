f201a6f05e6333b7ad2a17465be5f6fd4ff7b23c
LeeDayday <dayoon0905@gmail.com>
2023-09-09 23:59:43 +0900

    feat: NewUserInfo model 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class NewUserInfo(models.Model):
+    student_id = models.CharField(primary_key=True, max_length=10)
+    password = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    year = models.IntegerField()
+    major_status = models.CharField(max_length=10)
+    major = models.CharField(max_length=50)
+    sub_major = models.CharField(max_length=50, blank=True, null=True)
 
