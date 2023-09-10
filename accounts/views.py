8e6bb1e9be5224172d44d700217c0f7b884902a7
LeeDayday <dayoon0905@gmail.com>
2023-09-11 01:37:07 +0900

    feat: 대양휴머니티 인증 실패로 인한 로그인 실패시 예외 처리 및 오류 수정

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
 from .models import NewUserInfo
-from ..utils.crawler import get_user_info
+from utils.crawler import get_user_info
 from django.contrib import messages
 from bcrypt import hashpw, gensalt
 from django.middleware.csrf import get_token
-
+from django.shortcuts import redirect
 
 def f_certify(request):
     # 입력 받은 id/pw 꺼낸다
@@ -17,9 +17,12 @@ def f_certify(request):
     # id/pw 이 틀린 경우, 가입이 불가한 재학생인 경우
     if user_info == 'err_auth':
         messages.error(request, "세종대학교 포털 ID/PW를 다시 확인하세요. / 재외국민전형 입학자, 계약학과, 편입생은 로그인이 불가합니다.")
+        return False
+
     # 대양 휴머니티 칼리지 사이트의 오류인 경우
     elif user_info == 'err_server':
         messages.error(request, "대양 휴머니티 칼리지 서버 오류. 잠시 후 시도해주세요.")
+        return False
 
     # 비밀번호 암호화
     user_pw = hashpw(user_pw.encode('utf-8'), gensalt())
@@ -36,6 +39,7 @@ def f_certify(request):
 
     new_user.save()
 
+    # return redirect('/index/')
 
 def f_login(request):
     # csrf token 확인
@@ -43,13 +47,21 @@ def f_login(request):
     # 입력 받은 id/pw 꺼낸다
     user_id = request.POST.get('id')
     user_pw = request.POST.get('pw')
-
     # Model에서 행 추출
     user_row = NewUserInfo.objects.filter(student_id=user_id)
 
     # 처음 로그인한 경우 대양 휴머니티 인증 작업 진행
     if not user_row.exists():
-        f_certify(request, user_id, user_pw)
+        result = f_certify(request)
+        # 회원 가입 후 model에서 행 추출
+        user_row = NewUserInfo.objects.filter(student_id=user_id)
+
+    # 대양 휴머니티 칼리지 인증 실패한 경우
+    if result is False:
+        return redirect('/login/')
+
+    # 입력받은 pw와 db에 저장된 pw 비교 (user_pw 암호화 후 비교)
+    # user_pw =
 
 
 
