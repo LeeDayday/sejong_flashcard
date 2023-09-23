d5e4c2272c47c549eb239b75c1f7512c25730634
LeeDayday <dayoon0905@gmail.com>
2023-09-23 18:23:23 +0900

    refactor: template 상속 적용 및 파일 이름 변경

--- utils/rendering.py
+++ utils/rendering.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 
 def r_login(request):
-    response = render(request, "login_page/loginPage.html")
+    response = render(request, "login.html")
     return response
+
 
 def r_home(request):
-    response = render(request, "main_page/main_page.html")
+    response = render(request, "main.html")
     return response
-
