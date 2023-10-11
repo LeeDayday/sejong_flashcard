738327785fcd45737916cce783b9bde25401c2f1
LeeDayday <dayoon0905@gmail.com>
2023-10-11 17:54:23 +0900

    feat: Deck 리스트 조회 API(DeckView) 생성

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from utils.rendering import *
-
+from quizzes.views import (DeckView)
 urlpatterns = [
-    path('myquiz/', r_quiz, name = 'r_quiz')
+    path('myquiz/', r_quiz, name='r_quiz'),
+    path('', DeckView.as_view())
 ]
--- quizzes/views.py
+++ quizzes/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
+from .models import Deck
+from .serializers import DeckSerializer
 
-# Create your views here.
-# ..
+
+class DeckView(APIView, PageNumberPagination):
+    """
+    Deck 리스트 조회
+    """
+    def get(self, request):
+        all_decks = Deck.objects.all()
+        page = self.paginate_queryset(all_decks, request)
+        serializer = DeckSerializer(page, many=True)
+        return self.get_paginated_response(serializer.data)
+
+
+
+
+
+
+
