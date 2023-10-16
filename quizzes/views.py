03106d34bf7db98e644a07478d1b0b979bcb6664
LeeDayday <dayoon0905@gmail.com>
2023-10-16 14:40:26 +0900

    feat: Deck 생성 API(DeckView) 생성

--- quizzes/views.py
+++ quizzes/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
 from .models import Deck
 from .serializers import DeckSerializer
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
 
 class DeckView(APIView, PageNumberPagination):
@@ -15,6 +16,19 @@ class DeckView(APIView, PageNumberPagination):
         page = self.paginate_queryset(all_decks, request)
         serializer = DeckSerializer(page, many=True)
         return self.get_paginated_response(serializer.data)
+
+    """
+    Deck 생성
+    """
+    def post(self, request):
+        data = request.data
+        data['owner'] = request.user.student_id
+        serializer = DeckSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save() # owner field를 채운 새로운 모델 인스턴스 생성 및 저장
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
 
 
 
