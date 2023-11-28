1fc5d46718b5e13bec5b9602cb95d4e4e7a5ddd9
solyrion <sde101573@gmail.com>
2023-11-28 19:46:46 +0900

    fix: 모델 수정

--- cal/models.py
+++ cal/models.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.db import models
 from django.contrib.auth.models import User
 from accounts.models import NewUserInfo
@@ -7,24 +5,24 @@ from django.utils import timezone
 from django.urls import reverse
 
 # Create your models here.
-# class Calendar(models.Model):
-#     owner = models.ForeignKey(NewUserInfo, on_delete=models.CASCADE)
-#     # content = models.ForeignKey(Content, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=50)
-#
-#     def __str__(self):
-#         return self.name
-#
-#     class Meta:
-#         db_table = "calendar"
+class Calendar(models.Model):
+    owner = models.ForeignKey(NewUserInfo, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "calendar"
+
 
 class Content(models.Model):
     # calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
     owner = models.ForeignKey(NewUserInfo, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    start_time = models.DateTimeField(default=timezone.now)
-    end_time = models.DateTimeField(default=timezone.now)
+    start_time = models.DateField(default=timezone.now)
+    end_time = models.DateField(default=timezone.now)
 
     def __str__(self):
         return self.title
