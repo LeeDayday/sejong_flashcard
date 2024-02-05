c58eb15c10b292ec3c4513d6f9a31179ce8bdfe7
LeeDayday <dayoon0905@gmail.com>
2024-02-05 15:55:21 +0900

    feat: swagger 기본 설정 추가

--- FALL2023_project/settings.py
+++ FALL2023_project/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
     # drf
     'rest_framework',
+    'drf_yasg',
 
 ]
 
--- FALL2023_project/urls.py
+++ FALL2023_project/urls.py
@@ -15,9 +15,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from accounts.views import *
 from utils.rendering import *
+from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="FALL2023 swagger API",
+        default_version="v1",
+        description="FALL2023을 위한 API 문서",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,4 +53,12 @@ urlpatterns = [
 
     # 캘린더 랜더링
     path('cal/', include('cal.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
